6c8bdab1c7e4826b450cb854f55f5efe49a876b3
sacred5011 <sacred5011@yahoo.com.tw>
2018-09-19 23:28:52 -0400

    Fix refresh problem

--- js/listening_phase/memc.js
+++ js/listening_phase/memc.js
@@ -1,6 +1,8 @@
 //Warning before existing
 // var AttemptLeave = 0
-window.onbeforeunload = function () { return "糟糕！別走！" };
+window.onbeforeunload = function() {
+    return "糟糕！別走！"
+};
 
 // if (window.onbeforeunload== !null) {
 //   AttemptLeave += 1;
@@ -9,15 +11,13 @@ window.onbeforeunload = function () { return "糟糕！別走！" };
 
 
 /* hide the questionires first */
-$(function () {
+$(function() {
     $('#MEMCscale').hide();
 });
 
 /* show the scale after click the MP3 file */
-$(function () {
-    $('#btn-play').on('click', function () {
-        $('#MEMCscale').delay(3000).show(300).scrollTop(0);
-    });
+$('#btn-play').on('click', function() {
+    $('#MEMCscale').delay(3000).show(300).scrollTop(0);
 });
 
 
@@ -72,8 +72,10 @@ var currentPage = 1 //沒有指導語頁面0
 var Response = {}
 var ResponseTime = {}
 var starttime, endtime
+var itemsPerPage = 25;
+Npage = 10;
 
-window.onload = function () {
+window.onload = function() {
     //get startTime
     starttime = performance.now()
     // prepare Object
@@ -82,61 +84,20 @@ window.onload = function () {
     }
 }
 
-function nextPage() {
-    if (validateSlider()) {
-        if (currentPage == Npage) {
-            // last page, do something
-
-        } else {
-            $('html, body').animate({
-                scrollTop: $("#page" + (currentPage + 1)).offset().top - ($(window).height() - $("#page" + (currentPage + 1)).outerHeight(true)) / 2
-            }, 200);
-            $(".page").addClass("Hide")
-            $("#page" + (currentPage + 1)).removeClass("Hide").fadeIn(500)
-            currentPage++
-            if (currentPage == Npage) {
-                $("#next").html("Finished")
-            }
-        }
-    }
-}
-
-
-
-
-// validate if response in currentPage
-function validateSlider() {
-    var validate = 0
-    $("#page" + currentPage + " .rangebar").each(function (index) {
-        var response = $(this).slider("option", "value")
-        if (response == null) {
-            $(this).prev(".itemcontent").addClass("forgethint")
-            validate++
-        } else {
-            // Response[$(this).data("item")] = response
-        }
-    });
-
-    if (validate != 0) {
-        $('html, body').animate({
-            scrollTop: $(".forgethint").offset().top - ($(window).height() - $(".forgethint").outerHeight(true)) / 2
-        }, 200);
-        return false
-    } else {
-        return true
-    }
-}
 
 //append items
 //start from 1 because no instruction
-$(function () {
+$(function() {
+    console.log(Npage)
     for (var i = 1; i <= Npage; i++) {
         $("form").prepend("<div class='page Hide' id='page" + i + "'></div>")
-        for (var j = 0; j < itemsPrePage; j++) {
-            var itemOrder = (i - 1) * itemsPrePage + (j + 1)
-            if (itemOrder <= Qlength) {
-                $("#page" + i).append("<div class='itembox'><div class='itemcontent'>" + itemOrder + ". " + MEMC[Qorder[itemOrder - 1]] + "</div><div data-item='" + (Qorder[itemOrder - 1]) + "' class='rangebar'></div></div>")
-            }
+        for (var j = 0; j < itemsPerPage; j++) {
+            //             var itemOrder = (i - 1) * itemsPrePage + (j + 1)
+            $("#page" + i).append("<div class='itembox'><div class='itemcontent'>" + (j + 1) + ". " + MEMC[j + 1] + "</div><div data-item='" + (j + 1) + "' class='rangebar'></div></div>")
+
+            //             if (itemOrder <= Qlength) {
+            //                 $("#page" + i).append("<div class='itembox'><div class='itemcontent'>" + itemOrder + ". " + MEMC[Qorder[itemOrder - 1]] + "</div><div data-item='" + (Qorder[itemOrder - 1]) + "' class='rangebar'></div></div>")
+            //             }
         }
     }
     $("#page1").removeClass("Hide").fadeIn(500)
@@ -146,15 +107,15 @@ $(function () {
 })
 
 //make rangebar
-$(function () {
-    $(".rangebar").each(function () {
+$(function() {
+    $(".rangebar").each(function() {
         $(this).empty().slider({
             value: null,
             min: 1,
             max: 5,
             range: "min",
             animate: "fast",
-            slide: function (event, ui) { //slide function supposes to be same as start
+            slide: function(event, ui) { //slide function supposes to be same as start
                 var item = $(this).data("item")
                 Response[item] = ui.value //store data
                 if ($(this).slider("option", "value") == null) {
@@ -166,7 +127,7 @@ $(function () {
                 $(this).prev(".itemcontent").removeClass("forgethint")
                 return $("[data-item='" + item + "'] .ui-slider-handle").html(ui.value);
             },
-            start: function (event, ui) {
+            start: function(event, ui) {
                 var item = $(this).data("item")
                 Response[item] = ui.value //store data
                 if ($(this).slider("option", "value") == null) {
@@ -178,7 +139,7 @@ $(function () {
                 $(this).prev(".itemcontent").removeClass("forgethint")
                 return $("[data-item='" + item + "'] .ui-slider-handle").html(ui.value);
             },
-            stop: function (event, ui) {
+            stop: function(event, ui) {
 
             }
         });
@@ -189,7 +150,13 @@ $(function () {
         var vals = opt.max - opt.min;
 
         //label character you want to show
-        var indexvalue = { "1": "Not at all", "2": "Somewhat", "3": "Moderately", "4": "Quite a lot", "5": "Very much" }
+        var indexvalue = {
+            "1": "Not at all",
+            "2": "Somewhat",
+            "3": "Moderately",
+            "4": "Quite a lot",
+            "5": "Very much"
+        }
         // Space out values
         for (var i = 0; i <= vals; i++) {
             var el = $('<label>' + indexvalue[i + 1] + '</label>').css({
@@ -203,14 +170,14 @@ $(function () {
 
 function reloadRangebar() {
 
-    $(".rangebar").each(function () {
+    $(".rangebar").each(function() {
         $(this).empty().slider({
             value: null,
             min: 1,
             max: 5,
             range: "min",
             animate: "fast",
-            slide: function (event, ui) { //slide function supposes to be same as start
+            slide: function(event, ui) { //slide function supposes to be same as start
                 var item = $(this).data("item")
                 Response[item] = ui.value //store data
                 if ($(this).slider("option", "value") == null) {
@@ -222,7 +189,7 @@ function reloadRangebar() {
                 $(this).prev(".itemcontent").removeClass("forgethint")
                 return $("[data-item='" + item + "'] .ui-slider-handle").html(ui.value);
             },
-            start: function (event, ui) {
+            start: function(event, ui) {
                 var item = $(this).data("item")
                 Response[item] = ui.value //store data
                 if ($(this).slider("option", "value") == null) {
@@ -234,7 +201,7 @@ function reloadRangebar() {
                 $(this).prev(".itemcontent").removeClass("forgethint")
                 return $("[data-item='" + item + "'] .ui-slider-handle").html(ui.value);
             },
-            stop: function (event, ui) {
+            stop: function(event, ui) {
 
             }
         });
@@ -245,7 +212,13 @@ function reloadRangebar() {
         var vals = opt.max - opt.min;
 
         //label character you want to show
-        var indexvalue = { "1": "Not at all", "2": "Somewhat", "3": "Moderately", "4": "Quite a lot", "5": "Very much" }
+        var indexvalue = {
+            "1": "Not at all",
+            "2": "Somewhat",
+            "3": "Moderately",
+            "4": "Quite a lot",
+            "5": "Very much"
+        }
         // Space out values
         for (var i = 0; i <= vals; i++) {
             var el = $('<label>' + indexvalue[i + 1] + '</label>').css({
@@ -267,8 +240,8 @@ function reloadRangebar() {
 var CommentsArray = [];
 
 
-$(function () {
-    $('#SendComments').on('click', function () {
+$(function() {
+    $('#SendComments').on('click', function() {
         var NewComments = $('#MusicComments').val();
         var NewTAndC = [formatTime(Spectrum.getCurrentTime()), $('#MusicComments').val()];
         var currentProgress = Spectrum.getCurrentTime() / Spectrum.getDuration();
@@ -285,11 +258,11 @@ $(function () {
 
 
 //Preventing send the data repeatedly
-$("#MEMCscale").submit(function () {
+$("#MEMCscale").submit(function() {
     $("#MEMCbutton").prop('disabled', true);
 });
 
 //Submitting the form
-$(".submit_btn").click(function () {
+$(".submit_btn").click(function() {
     window.onbeforeunload = null; //prevent window.onbeforeunload event being triggered by button
 });
--- js/listening_phase/music.js
+++ js/listening_phase/music.js
@@ -3,181 +3,139 @@
 // 
 
 
-$(function() {
+// $(function() {
 
 
-    var buttons = {
-        play: document.getElementById("btn-play"),
-        pause: document.getElementById("btn-pause"),
-        Restart: document.getElementById("btn-Restart")
-    };
+var buttons = {
+    play: document.getElementById("btn-play"),
+    pause: document.getElementById("btn-pause"),
+    Restart: document.getElementById("btn-Restart")
+};
 
-    // Create an instance of wave surfer with its configuration
-    var Spectrum = WaveSurfer.create({
-        container: '#audio-spectrum',
-        waveColor: 'violet',
-        progressColor: 'purple',
-        height: 128,
-        barHeight: 1,
-        // normalize: true, // positive correlate with volume
-        barWidth: 4,
-        minPxPerSec: 25
-    });
+// Create an instance of wave surfer with its configuration
+var Spectrum = WaveSurfer.create({
+    container: '#audio-spectrum',
+    waveColor: 'violet',
+    progressColor: 'purple',
+    height: 128,
+    barHeight: 1,
+    // normalize: true, // positive correlate with volume
+    barWidth: 4,
+    minPxPerSec: 25
+});
 
-    // Handle Play button
-    buttons.play.addEventListener("click", function() {
-        Spectrum.play();
+// Handle Play button
+buttons.play.addEventListener("click", function() {
+    Spectrum.play();
 
-        // Enable/Disable respectively buttons
-        buttons.Restart.disabled = false;
-        buttons.pause.disabled = false;
-        buttons.play.disabled = true;
-    }, false);
+    // Enable/Disable respectively buttons
+    buttons.Restart.disabled = false;
+    buttons.pause.disabled = false;
+    buttons.play.disabled = true;
+}, false);
 
-    // Handle Pause button
-    buttons.pause.addEventListener("click", function() {
-        Spectrum.pause();
+// Handle Pause button
+buttons.pause.addEventListener("click", function() {
+    Spectrum.pause();
 
-        // Enable/Disable respectively buttons
-        buttons.pause.disabled = true;
-        buttons.play.disabled = false;
-    }, false);
-
-
-    // Handle Restart button
-    buttons.Restart.addEventListener("click", function() {
-        Spectrum.stop();
-
-        // Enable/Disable respectively buttons
-        buttons.pause.disabled = true;
-        buttons.play.disabled = false;
-        buttons.Restart.disabled = true;
-    }, false);
+    // Enable/Disable respectively buttons
+    buttons.pause.disabled = true;
+    buttons.play.disabled = false;
+}, false);
 
 
-    // Add a listener to enable the play button once it's ready
-    Spectrum.on('ready', function() {
-        buttons.play.disabled = false;
-    });
+// Handle Restart button
+buttons.Restart.addEventListener("click", function() {
+    Spectrum.stop();
 
-    // If you want a responsive mode (so when the user resizes the window)
-    // the spectrum will be still playable
-    window.addEventListener("resize", function() {
-        // Get the current progress according to the cursor position
-        var currentProgress = Spectrum.getCurrentTime() / Spectrum.getDuration();
-
-        // Reset graph
-        Spectrum.empty();
-        Spectrum.drawBuffer();
-        // Set original position
-        Spectrum.seekTo(currentProgress);
-
-        // Enable/Disable respectively buttons
-        buttons.pause.disabled = true;
-        buttons.play.disabled = false;
-        buttons.Restart.disabled = false;
-    }, false);
+    // Enable/Disable respectively buttons
+    buttons.pause.disabled = true;
+    buttons.play.disabled = false;
+    buttons.Restart.disabled = true;
+}, false);
 
 
-    //--------------------------------------- record listening time ---------------------------------------
+// Add a listener to enable the play button once it's ready
+Spectrum.on('ready', function() {
+    buttons.play.disabled = false;
+});
+
+// If you want a responsive mode (so when the user resizes the window)
+// the spectrum will be still playable
+window.addEventListener("resize", function() {
+    // Get the current progress according to the cursor position
+    var currentProgress = Spectrum.getCurrentTime() / Spectrum.getDuration();
+
+    // Reset graph
+    Spectrum.empty();
+    Spectrum.drawBuffer();
+    // Set original position
+    Spectrum.seekTo(currentProgress);
+
+    // Enable/Disable respectively buttons
+    buttons.pause.disabled = true;
+    buttons.play.disabled = false;
+    buttons.Restart.disabled = false;
+}, false);
 
 
-
-    //     var songTime = 0;
-    //     setInterval(function() {
-    //         if (Spectrum.isPlaying() == true) {
-    //             //console.log("Sucessful record");
-    //             songTime = (Number(songTime) + 0.01).toFixed(2); // Number() change the string into number 
-    //             $('#TimeCount').text("Play Time" + songTime);
-    //         }
-    //     }, 10);
-
-    // Set the time information in "X:XX" form
-    var formatTime = function(time) {
-        return [
-            Math.floor((time % 3600) / 60), // minutes
-            ('00' + Math.floor(time % 60)).slice(-2) // seconds
-        ].join(':');
-    };
-
-    // // Show current time
-    //   Spectrum.on('audioprocess', function () {
-    //   //console.log("Sucessfully assess current time");
-    //   $('.waveform-counter').text( "Current Time" + formatTime(Spectrum.getCurrentTime()) );
-    //   });
-
-    // // Show clip duration
-    //   Spectrum.on('ready', function () {
-    //   //console.log("Sucessfully assess duration");
-    //   $('.waveform-duration').text( "Duration"+ formatTime(Spectrum.getDuration()) );
-    //   });
-
-    //--------------------------------------- Update for time information ---------------------------------------
-    Spectrum.on('ready', function() {
-        //console.log("Sucessfully assess current time");
-        $('#TimeInformation').text(formatTime(Spectrum.getCurrentTime()) + " / " + formatTime(Spectrum.getDuration()));
-    });
-
-
-    Spectrum.on('audioprocess', function() {
-        //console.log("Sucessfully assess current time");
-        $('#TimeInformation').text(formatTime(Spectrum.getCurrentTime()) + " / " + formatTime(Spectrum.getDuration()));
-    });
-
-
-    Spectrum.on('seek', function() {
-        //console.log("Sucessfully assess current time");
-        $('#TimeInformation').text(formatTime(Spectrum.getCurrentTime()) + " / " + formatTime(Spectrum.getDuration()));
-    });
+//--------------------------------------- record listening time ---------------------------------------
 
 
 
-    // 一開始先 load 第0首
-    var idx = 0;
-    var random_music = sampleMusic();
-    Spectrum.load(random_music[idx]); // random assign music here 
+//     var songTime = 0;
+//     setInterval(function() {
+//         if (Spectrum.isPlaying() == true) {
+//             //console.log("Sucessful record");
+//             songTime = (Number(songTime) + 0.01).toFixed(2); // Number() change the string into number 
+//             $('#TimeCount').text("Play Time" + songTime);
+//         }
+//     }, 10);
 
-    //  記錄每一首歌的時間
-    var listeningTime = {};
-    setInterval(function() {
-        if (Spectrum.isPlaying() === true) {
+// Set the time information in "X:XX" form
+var formatTime = function(time) {
+    return [
+        Math.floor((time % 3600) / 60), // minutes
+        ('00' + Math.floor(time % 60)).slice(-2) // seconds
+    ].join(':');
+};
 
-            // 如果music 已經有紀錄，就取出之前紀錄的時間，不然songTime就設為0
-            let music = random_music[idx];
-            let songTime = (music in listeningTime) ? listeningTime[music] : 0;
+// // Show current time
+//   Spectrum.on('audioprocess', function () {
+//   //console.log("Sucessfully assess current time");
+//   $('.waveform-counter').text( "Current Time" + formatTime(Spectrum.getCurrentTime()) );
+//   });
 
-            // 累加時間
-            //console.log("Sucessful record");
-            songTime = (Number(songTime) + 0.01).toFixed(2); // Number() change the string into number 
-            $('#TimeCount').text("Play Time" + songTime);
+// // Show clip duration
+//   Spectrum.on('ready', function () {
+//   //console.log("Sucessfully assess duration");
+//   $('.waveform-duration').text( "Duration"+ formatTime(Spectrum.getDuration()) );
+//   });
 
-            // 把時間存回去            
-            listeningTime[music] = songTime;
-            console.log(listeningTime);
-        }
-    }, 10);
-
-
-    $("#next").click(function() {
-
-        // TODO: 上傳分數 要吃music ID 當 參數
+//--------------------------------------- Update for time information ---------------------------------------
+Spectrum.on('ready', function() {
+    //console.log("Sucessfully assess current time");
+    $('#TimeInformation').text(formatTime(Spectrum.getCurrentTime()) + " / " + formatTime(Spectrum.getDuration()));
+});
 
 
-        // TODO: 上傳完要做判斷，如果是第五個就是問卷結束
-        if (idx === 5) {
+Spectrum.on('audioprocess', function() {
+    //console.log("Sucessfully assess current time");
+    $('#TimeInformation').text(formatTime(Spectrum.getCurrentTime()) + " / " + formatTime(Spectrum.getDuration()));
+});
 
-        }
 
-        // TODO: 繼續 清掉range bar
-        idx += 1;
-        Spectrum.load(random_music[idx]); // random assign music here 
-        reloadRangebar();
-        $('#MEMCscale').hide();
-
-    });
+Spectrum.on('seek', function() {
+    //console.log("Sucessfully assess current time");
+    $('#TimeInformation').text(formatTime(Spectrum.getCurrentTime()) + " / " + formatTime(Spectrum.getDuration()));
+});
 
 
 
 
 
-})
+
+
+
+
+// })
