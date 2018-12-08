6ecdbfbd79dacf17e41bbc0a0e3cd0c546377f0b
Halcyonsings <r05227104@ntu.edu.tw>
2018-12-08 19:58:48 +0800

    listening phase correct

---
+++ js/2_listening_phase/index.js
@@ -0,0 +1,99 @@
+/**
+ * @author Doublebite r05921030@g.ntu.edu.tw
+ * @fileoverview - 使用music.js跟memc.js初始化的問卷跟音樂物件以執行問卷功能的script
+ */
+
+
+
+
+// The umber of questions to render and the current page
+// Declare the variable for the play list of musics
+let currentTrial = 1;
+let playList;
+let allAnswers = {};
+
+// Render the questions
+MEMC.render("memc");
+
+
+// Get the music list(a csv file) from server, save to our play list, and then play the first song
+$.ajax({
+
+        url: "clips_information.csv",
+        dataType: "text",
+
+    })
+    .done(function(csv) {
+
+        // Get an object with each music item taking its ID as key.
+        const musicItems = $.csv.toObjects(csv) // Returns an array:  Array[0] = {clips_id: "1", pairs: "1", author: "AI", music_file: "music/AIBachHCII.mp3"}
+        console.log("Music items", musicItems);
+
+        // Ramdon sample two numbers in range 1 to 8, and get the musics which pair id is in the sampled IDs
+        // Sample the control music.
+        let sampledIDs = shuffle([1, 2, 3, 4, 5, 6, 7, 8]).slice(0, 2);
+        let sampledMusics = musicItems.filter(item => sampledIDs.includes(+item.pairs));
+        let controlMusic = musicItems[shuffle([16, 17])[0]];
+
+        // Set our play list
+        playList = sampledMusics.concat(controlMusic).map(item => item.music_file);
+        playList = shuffle(playList);
+        console.log("Play list", playList);
+
+        // Play the first song
+        Spectrum.load(playList[currentTrial - 1]);
+
+    });
+
+
+
+/**
+ * The function to execute when the "Next" button is clicked.
+ * @param {null} 
+ * @return {null}
+ */
+function nextStep() {
+    // Get answers and 
+    let phase = MEMC.getCurrentPhase();
+    let phaseAnswers = MEMC.getAnswers(phase);
+
+    // Check answers, submit the answers and jump to next page. 
+    if (Object.values(phaseAnswers).includes(null)) {
+        // If the the answers contain null, alert the user.
+        alert("Please finish questions: \n" + getAllIndexes(Object.values(phaseAnswers), null).map(e => e + 1).join(", "));
+    } else {
+
+        MEMC.toggle();
+        if (phase === "phase1")
+            return false;
+        else {
+            let answers = MEMC.getAnswers();
+            // Do something to send these answers to server.
+            console.log(playList[currentTrial - 1], answers);
+            allAnswers[playList[currentTrial - 1]] = answers;
+
+            // Jump to next page
+            if (currentTrial == playList.length) {
+                // The last trial is over. Do something.
+                console.log(allAnswers);
+            } else {
+                currentTrial++;
+                Spectrum.load(playList[currentTrial - 1]);
+                MEMC.shuffle();
+                $('#memc').hide();
+                $('#MEMCscale').hide();
+            }
+        }
+    }
+}
+
+/**
+ * Get the indexes of the input val occuring on the input array.
+ */
+function getAllIndexes(arr, val) {
+    var indexes = [];
+    for (let i = 0; i < arr.length; i++)
+        if (arr[i] === val)
+            indexes.push(i);
+    return indexes;
+}
---
+++ js/2_listening_phase/music.js
@@ -0,0 +1,141 @@
+//--------------------------------------- wavesurfer variables ---------------------------------------
+// Store the 3 buttons in some object
+// 
+
+
+// $(function() {
+
+
+var buttons = {
+    play: document.getElementById("btn-play"),
+    pause: document.getElementById("btn-pause"),
+    Restart: document.getElementById("btn-Restart")
+};
+
+// Create an instance of wave surfer with its configuration
+var Spectrum = WaveSurfer.create({
+    container: '#audio-spectrum',
+    waveColor: 'violet',
+    progressColor: 'purple',
+    height: 80,
+    barHeight: 2,
+    // normalize: true, // positive correlate with volume
+    barWidth: 4,
+    minPxPerSec: 25
+});
+
+// Handle Play button
+buttons.play.addEventListener("click", function() {
+    Spectrum.play();
+
+    // Enable/Disable respectively buttons
+    buttons.Restart.disabled = false;
+    buttons.pause.disabled = false;
+    buttons.play.disabled = true;
+}, false);
+
+// Handle Pause button
+buttons.pause.addEventListener("click", function() {
+    Spectrum.pause();
+
+    // Enable/Disable respectively buttons
+    buttons.pause.disabled = true;
+    buttons.play.disabled = false;
+}, false);
+
+
+// Handle Restart button
+buttons.Restart.addEventListener("click", function() {
+    Spectrum.stop();
+
+    // Enable/Disable respectively buttons
+    buttons.pause.disabled = true;
+    buttons.play.disabled = false;
+    buttons.Restart.disabled = true;
+}, false);
+
+
+// Add a listener to enable the play button once it's ready
+Spectrum.on('ready', function() {
+    buttons.play.disabled = false;
+});
+
+// If you want a responsive mode (so when the user resizes the window)
+// the spectrum will be still playable
+window.addEventListener("resize", function() {
+    // Get the current progress according to the cursor position
+    var currentProgress = Spectrum.getCurrentTime() / Spectrum.getDuration();
+
+    // Reset graph
+    Spectrum.empty();
+    Spectrum.drawBuffer();
+    // Set original position
+    Spectrum.seekTo(currentProgress);
+
+    // Enable/Disable respectively buttons
+    buttons.pause.disabled = true;
+    buttons.play.disabled = false;
+    buttons.Restart.disabled = false;
+}, false);
+
+
+//--------------------------------------- record listening time ---------------------------------------
+
+
+
+//     var songTime = 0;
+//     setInterval(function() {
+//         if (Spectrum.isPlaying() == true) {
+//             //console.log("Sucessful record");
+//             songTime = (Number(songTime) + 0.01).toFixed(2); // Number() change the string into number 
+//             $('#TimeCount').text("Play Time" + songTime);
+//         }
+//     }, 10);
+
+// Set the time information in "X:XX" form
+var formatTime = function(time) {
+    return [
+        Math.floor((time % 3600) / 60), // minutes
+        ('00' + Math.floor(time % 60)).slice(-2) // seconds
+    ].join(':');
+};
+
+// // Show current time
+//   Spectrum.on('audioprocess', function () {
+//   //console.log("Sucessfully assess current time");
+//   $('.waveform-counter').text( "Current Time" + formatTime(Spectrum.getCurrentTime()) );
+//   });
+
+// // Show clip duration
+//   Spectrum.on('ready', function () {
+//   //console.log("Sucessfully assess duration");
+//   $('.waveform-duration').text( "Duration"+ formatTime(Spectrum.getDuration()) );
+//   });
+
+//--------------------------------------- Update for time information ---------------------------------------
+Spectrum.on('ready', function() {
+    //console.log("Sucessfully assess current time");
+    $('#TimeInformation').text(formatTime(Spectrum.getCurrentTime()) + " / " + formatTime(Spectrum.getDuration()));
+});
+
+
+Spectrum.on('audioprocess', function() {
+    //console.log("Sucessfully assess current time");
+    $('#TimeInformation').text(formatTime(Spectrum.getCurrentTime()) + " / " + formatTime(Spectrum.getDuration()));
+});
+
+
+Spectrum.on('seek', function() {
+    //console.log("Sucessfully assess current time");
+    $('#TimeInformation').text(formatTime(Spectrum.getCurrentTime()) + " / " + formatTime(Spectrum.getDuration()));
+});
+
+
+
+
+
+
+
+
+
+// })
